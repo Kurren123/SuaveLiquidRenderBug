30091ce737a85394706bdc684763cd067dfafd04
Kurren <kurren.nischal@codis.co.uk>
2017-10-17 17:34:43 +0100

    initial

---
+++ ConsoleApplication3/Program.fs
@@ -0,0 +1,12 @@
+﻿type M1 =
+    { name : string }
+
+let subject =
+    Suave.DotLiquid.renderPageString "Hi {{ model.name }}" { M1.name = "haf3" }
+    |> Async.RunSynchronously
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" subject
+    System.Console.ReadLine() |> ignore
+    0
